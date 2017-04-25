4d0c7ad4e28d6bbfb5a2ad1a4323af2c4e725675
Adam Berelowitz <aberelo2@cs.binghamton.edu>
2017-04-25 16:29:37 -0400

    :x

--- ssfs.cpp
+++ ssfs.cpp
@@ -45,14 +45,32 @@ int main(int argc, char * argv[]){
     Wait for commands from the user from command line.
  */
   while(true){
+    std::string input;
+    cin >> input;
 
-
-
+    if(input.compare(0, 7, "CREATE ") == 0 && input.size() > 7){
+      create(input.substr(7, input.size()));
+    } else if(input.compare(0, 7, "IMPORT ") == 0 && input.size() > 7){
+      import(input.substr(7, input.size()));
+    } else if(input.compare(0, 4, "CAT ") == 0 && input.size() > 4){
+      cat(input.substr(4, input.size()));
+    } else if(input.compare(0, 6, "WRITE ") == 0 && input.size() > 6){
+      /* NEED MORE HERE FOR FORMATTING FOR EXTRA INPUT */
+      write(input.substr(6, input.size()));
+    } else if(input.compare(0, 7, "DELETE ") == 0 && input.size() > 7){
+      f_delete(input.substr(7, input.size()));
+    } else if(input.compare(0, 5, "READ ") == 0 && input.size() > 5){
+      /* NEED MORE HERE FOR FORMATTING FOR EXTRA INPUT */
+      read(input.substr(5, input.size()));
+    } else if(input.compare(0, 4, "LIST") == 0){
+      list_files();
+    } else if(input.compare(0, 8, "SHUTDOWN") == 0){
+      shutdown();
+    }
   }
 
   return 0;
 }
-
 
 bool all_disk_op_valid(std::string * disk_ops, int disk_op_array_size){
   for(int i = 0; i < disk_op_array_size; i++){
@@ -62,5 +80,22 @@ bool all_disk_op_valid(std::string * disk_ops, int disk_op_array_size){
       return false;
   }
   return true;
+}
+
+
+
+void create(std::string file_name){
+  
+
+}
+
+
+void import(std::string file_name){
+
+
+}
+
+void cat(std::string file_name){
+
 
 }
