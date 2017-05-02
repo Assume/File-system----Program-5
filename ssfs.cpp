9ec8fff37c0e2b73ca8c8ac26284a329194a8c30
Aiden Cullo <culloaiden3@gmail.com>
2017-05-01 20:19:31 -0400

    implementing shared memory

--- ssfs.cpp
+++ ssfs.cpp
@@ -19,38 +19,38 @@ int main(int argc, char * argv[]){
 
 	// SANITIZE INPUT 1,2,3,4 text files and at least write seudocode for creating threads
 	if(argc == 1){
-		std::cout << "No disk file name passed. Exiting." << std::endl;
+		perror("No disk file name passed. Exiting.");
 		return -1;
 	}
 
 	if(argc > 6){
-		std::cout  << "Invalid number of arguments passed. Exiting." std::endl;
+		perror("Invalid number of arguments passed. Exiting.");
 		return -1;
 	}
 
-	std::string disk_file_name = argv[0];
+	std::string disk_file_name = argv[1];
 	std::ofstream disk_file;
 
 	disk_file.open(disk_file_name);
 
 	//Verify disk file exists
 	if(!disk_file.is_open()){
-		std::cout << "Disk file does not exist. Exiting." << std::endl;
+		perror("Disk file does not exist. Exiting.");
 		return -1;
 	}
 
-	num_disk_op_extra = argc - 1;
+	num_disk_op_extra = argc - 2;
 
-	threads = new p_thread[num_disk_op_extra];
+	p_thread threads[num_disk_op_extra];
 
-	disk_op_threads = new std::string[num_disk_op_extra];
+	std::string disk_op_threads[num_disk_op_extra];
 
 	for(int i = 0; i < num_disk_op_extra; i++)
-		disk_op_threads[i] = argv[i+2];
+		disk_op_threads[i] = argv[i + 2];
 
 	//Verify that all passed disk op names are valid
 	if(!all_disk_op_valid(disk_op_threads, num_disk_op_extra)){
-		std::cout << "One or more disk op file names are invalid. Exiting." << std::endl;
+		perror("One or more disk op file names are invalid. Exiting.");
 		return -1;
 	}
 
